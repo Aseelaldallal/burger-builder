0c2407f77b724eca8f2fefcaffa1e63a02158c9d
unknown <aseelaldallal@gmail.com>
2017-11-26 23:06:41 -0500

    bug fix: add key to li

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,7 +5,7 @@ import Auxillary from '../../../hoc/Auxillary';
 const orderSummary = (props) => {
 
     const ingredientSummary = Object.keys(props.ingredients).map((currentValue)=> {
-        return (<li><span style={{textTransform: 'capitalize'}}>{currentValue}</span>: {props.ingredients[currentValue]}</li>);
+        return (<li key={currentValue}><span style={{textTransform: 'capitalize'}}>{currentValue}</span>: {props.ingredients[currentValue]}</li>);
     });
 
     return (
