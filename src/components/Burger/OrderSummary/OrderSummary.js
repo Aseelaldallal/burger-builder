0baefee5c5e0034a411f14e1769e040b43d62512
unknown <aseelaldallal@gmail.com>
2017-11-26 23:03:56 -0500

    create OrderSummary file

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Auxillary from '../../../hoc/Auxillary';
+
+
+const orderSummary = (props) => {
+
+    const ingredientSummary = Object.keys(props.ingredients).map((currentValue)=> {
+        return (<li><span style={{textTransform: 'capitalize'}}>{currentValue}</span>: {props.ingredients[currentValue]}</li>);
+    });
+
+    return (
+        <Auxillary>
+            <h3>Your Order</h3>
+            <p> A delicious burger with the following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Auxillary>
+    )
+}
+
+
+
+export default orderSummary; 
