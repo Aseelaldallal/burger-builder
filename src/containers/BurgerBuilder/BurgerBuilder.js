26d085608a73425a55a58ac24c4c1564fbd8975b
unknown <aseelaldallal@gmail.com>
2017-11-26 23:28:17 -0500

    Close modal when backdrop clicked

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,6 +64,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         const disabledInfo = {...this.state.ingredients }
         for(let key in disabledInfo) {
@@ -71,7 +75,7 @@ class BurgerBuilder extends Component {
         } 
         return(
             <Auxillary>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
