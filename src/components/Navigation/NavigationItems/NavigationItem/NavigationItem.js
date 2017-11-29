c0f8c385d81ad6b107ac948c028bae51497b3f6b
unknown <aseelaldallal@gmail.com>
2017-11-29 11:39:48 -0500

    update navigation items

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -6,7 +6,8 @@ import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a href="/">A Link</a>
+        <a href={props.link} 
+           className={props.active ? classes.active : null }>{props.children}</a>
     </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import classes from "./NavigationItems.css";
+import navigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = () => (
     <ul classes={classes.NavigationItems}>
-        
+        <NavigationItem link="/" active> Burger Builder</NavigationItem>
+        <NavigationItem link="/"> Checkout </NavigationItem>                
     </ul>
 );
 
