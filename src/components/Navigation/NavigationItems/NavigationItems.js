28584010745b80535883c11e4d8274ad56e5abd3
unknown <aseelaldallal@gmail.com>
2017-11-29 11:35:26 -0500

    create and style navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from "./NavigationItems.css";
+
+const navigationItems = () => (
+    <ul classes={classes.NavigationItems}>
+        
+    </ul>
+);
+
+export default navigationItems;
+
