b48eb7d8c8e62d8bbdd3465894155ee38fbde27c
unknown <aseelaldallal@gmail.com>
2017-12-12 09:46:11 -0500

    create order.js

---
+++ src/components/Order/Order.js
@@ -0,0 +1,15 @@
+
+
+
+
+import React from 'react';
+import classes from './Order.css';
+
+const order = (props) => (
+    <div className={clases.Order}>
+        <p> Ingredients: Salad(1) </p>
+        <p> Price: <strong> CAD 4.44 </strong></p>
+    </div>
+)
+
+export default order;
