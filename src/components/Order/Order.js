e2ff4079f87af2d6ac7e8eee8938ec99c6029504
unknown <aseelaldallal@gmail.com>
2017-12-12 09:52:58 -0500

    bug fix

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -6,7 +6,7 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => (
-    <div className={clases.Order}>
+    <div className={classes.Order}>
         <p> Ingredients: Salad(1) </p>
         <p> Price: <strong> CAD 4.44 </strong></p>
     </div>
