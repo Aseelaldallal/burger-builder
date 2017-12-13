83211ad6b10de88f2516c43cd6c780c01ebc0d2c
unknown <aseelaldallal@gmail.com>
2017-12-13 18:00:34 -0500

    add input validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }, 
+                valid: false
             }, 
             street: {
                 elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }, 
+                valid: false
             },
             postalCode: {
                 elementType: 'input',
@@ -32,7 +40,13 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Postal Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 6
+                }, 
+                valid: false
             },
             country: {
                 elementType: 'input',
@@ -40,7 +54,11 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }, 
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -48,7 +66,11 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Email'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                }, 
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -63,12 +85,29 @@ class ContactData extends Component {
         }
     }
 
+    checkValidity = (value, rules) => {
+        let isValid = true;
+        if(rules.required) {
+            isValid = (value.trim() !=='') && isValid;
+        }
+        if(rules.minLength) {
+            isValid = (value.replace(/ /g,'').length >= rules.minLength) && isValid;
+        }
+        if(rules.maxLength) {
+            isValid = (value.replace(/ /g,'').length <= rules.maxLength) && isValid;
+        }
+        return isValid;
+    }
+
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {...this.state.orderForm};
         const updatedFormElement = {
             ...updatedOrderForm[inputIdentifier]
         }
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        console.log(updatedFormElement);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         this.setState({
             orderForm: updatedOrderForm
