78b8ad5728433094e259f0d3c0276755eebcbdae
unknown <aseelaldallal@gmail.com>
2017-12-21 12:09:30 -0500

    add authentication route

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact> Burger Builder</NavigationItem>
-        <NavigationItem link="/Orders"> Orders </NavigationItem>                
+        <NavigationItem link="/Orders"> Orders </NavigationItem>
+        <NavigationItem link="/auth"> Sign In </NavigationItem>                    
     </ul>
 );
 
