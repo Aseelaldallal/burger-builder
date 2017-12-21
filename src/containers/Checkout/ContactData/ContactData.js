a9b91b077b31aad503a6092b9e718bd861aa6883
unknown <aseelaldallal@gmail.com>
2017-12-21 11:58:41 -0500

    Add email, isNumeric validation rules

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -73,7 +73,8 @@ class ContactData extends Component {
                 },
                 value: '',
                 validation: {
-                    required: true
+                    required: true,
+                    isEmail: true
                 }, 
                 valid: false,
                 touched: false
@@ -96,6 +97,11 @@ class ContactData extends Component {
 
     checkValidity = (value, rules) => {
         let isValid = true;
+
+        if (!rules) {
+            return true;
+        }
+
         if(rules.required) {
             isValid = (value.trim() !=='') && isValid;
         }
@@ -105,9 +111,21 @@ class ContactData extends Component {
         if(rules.maxLength) {
             isValid = (value.replace(/ /g,'').length <= rules.maxLength) && isValid;
         }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
 
+    
 
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {...this.state.orderForm};
