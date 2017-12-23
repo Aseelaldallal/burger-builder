636523837c24915a399b254a326d36650036bbe6
unknown <aseelaldallal@gmail.com>
2017-12-23 12:07:32 -0500

    combine auth reducer with other reducers

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 // APP 
 import App from './App';
@@ -23,7 +24,8 @@ import './index.css';
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+    order: orderReducer,
+    auth: authReducer
 }) // merge everything into one reducer
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // compose is a fallback
